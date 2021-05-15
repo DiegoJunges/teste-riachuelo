5ca9a25360196e851489feb1d3f2525d10de996d
Diego <diego.aviz@going2.com.br>
2021-05-15 19:33:43 -0300

    feat: created theme for switch feature

---
+++ src/styles/theme/dark.ts
@@ -0,0 +1,14 @@
+export default {
+  name: 'dark',
+  colors: {
+    body: '#161925',
+    text: {
+      primary: '#FFF',
+      secondary: '#23395b',
+    },
+    background: {
+      primary: '#eef4ed',
+      secondary: '#0b2545',
+    },
+  },
+};
---
+++ src/styles/theme/light.ts
@@ -0,0 +1,14 @@
+export default {
+  name: 'light',
+  colors: {
+    body: '#FAFAFA',
+    text: {
+      primary: '#303030',
+      secondary: '#23395b',
+    },
+    background: {
+      primary: '#eef4ed',
+      secondary: '#0b2545',
+    },
+  },
+};
