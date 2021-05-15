380dac4d657769d301b95900a6aaa93d78f75957
Diego <diego.aviz@going2.com.br>
2021-05-15 19:03:13 -0300

    feat: created global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  * { 
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    background: #303030;
+  }
+
+`
+
+export default GlobalStyles
